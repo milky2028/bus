1f132e850a8a2c18122aa111755c9622f334ced0
Tyler Gros <tylergross28@gmail.com>
2019-01-25 23:40:23 +0000

    it works!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,13 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+export const getBusPrediction = functions.https.onRequest(async (request, response) => {
+    try {
+        const url = 'http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=hx2E2juxTZSkkUqukqgn9h52J&stpid=6337&rt=60&format=json';
+        const prediction = await axios.get(url);
+        return response.status(200).send(prediction.data);
+    } catch (e) {
+        console.error(e);
+        return response.status(400).send();
+    }
 });
