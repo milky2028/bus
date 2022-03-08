f7971b5a85dd956480b9060a029c1dfe596c8d40
Tyler <tylergross28@gmail.com>
2022-03-08 09:36:24 -0500

    fix type errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,20 @@
-import * as functions from 'firebase-functions';
-import axios from 'axios';
+import * as functions from "firebase-functions";
+import axios from "axios";
 
-export const getBusPrediction = functions.https.onRequest(async (request, response) => {
+export const getBusPrediction = functions.https.onRequest(
+  async (_request, response) => {
     try {
-        response.set('Access-Control-Allow-Origin', '*');
-        response.set('Access-Control-Allow-Methods', 'GET');
-        response.set('Access-Control-Allow-Headers', 'Content-Type');
-        const url = 'http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=hx2E2juxTZSkkUqukqgn9h52J&stpid=6337&rt=60&format=json';
-        const prediction = await axios.get(url);
-        return response.status(200).send(prediction.data);
+      response.set("Access-Control-Allow-Origin", "*");
+      response.set("Access-Control-Allow-Methods", "GET");
+      response.set("Access-Control-Allow-Headers", "Content-Type");
+      const url =
+        "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=hx2E2juxTZSkkUqukqgn9h52J&stpid=6337&rt=60&format=json";
+      const prediction = await axios.get(url);
+
+      response.status(200).send(prediction.data);
     } catch (e) {
-        console.error(e);
-        return response.status(400).send();
+      console.error(e);
+      response.status(400).send();
     }
-});
+  }
+);
