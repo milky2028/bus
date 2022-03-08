eb06f34231af9d359c8c9d620549eb5a06224d33
Tyler <tylergross28@gmail.com>
2022-03-08 09:43:30 -0500

    respond to options requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,17 @@ import * as functions from "firebase-functions";
 import axios from "axios";
 
 export const getBusPrediction = functions.https.onRequest(
-  async (_request, response) => {
+  async (request, response) => {
     try {
+      if (request.method === "OPTIONS") {
+        response.status(200).send();
+        return;
+      }
+
       response.set("Access-Control-Allow-Origin", "*");
       response.set("Access-Control-Allow-Methods", "GET");
       response.set("Access-Control-Allow-Headers", "Content-Type");
+
       const url =
         "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=hx2E2juxTZSkkUqukqgn9h52J&stpid=6337&rt=60&format=json";
       const prediction = await axios.get(url);
